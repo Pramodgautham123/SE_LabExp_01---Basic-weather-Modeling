ccc0fa9a498455fb0e3d7116c3a439944d16e9ec
Pramodgautham123 <ppasupul4@gitam.in>
2025-07-14 15:22:02 +0530

    Create quadratic_file_multiple.py

---
+++ quadratic_file_multiple.py
@@ -0,0 +1,14 @@
+with open("input.txt", "r") as file:
+    for line in file:
+        a, b, c = map(float, line.strip().split())
+        D = b**2 - 4*a*c
+        print(f"\nEquation: {a}x² + {b}x + {c} = 0")
+        if D > 0:
+            x1 = (-b + math.sqrt(D)) / (2*a)
+            x2 = (-b - math.sqrt(D)) / (2*a)
+            print("Roots are real and distinct:", x1, x2)
+        elif D == 0:
+            x = -b / (2*a)
+            print("Roots are real and equal:", x)
+        else:
+            print("No real roots")
