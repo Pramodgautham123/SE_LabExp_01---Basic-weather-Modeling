ffd2809f4f6aa75ad05a8d2aa8048e9ca2100c01
Pramodgautham123 <ppasupul4@gitam.in>
2025-07-14 15:21:16 +0530

    Create quadratic_input.py

---
+++ quadratic_input.py
@@ -0,0 +1,14 @@
+try:
+    a = float(input("Enter a: "))
+    b = float(input("Enter b: "))
+    c = float(input("Enter c: "))
+
+    D = b**2 - 4*a*c
+    if D >= 0:
+        x1 = (-b + math.sqrt(D)) / (2*a)
+        x2 = (-b - math.sqrt(D)) / (2*a)
+        print("Roots:", x1, x2)
+    else:
+        print("No real roots")
+except ValueError:
+    print("Please enter valid numbers.")
